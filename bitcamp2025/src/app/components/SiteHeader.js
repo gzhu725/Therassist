c164527c2110625c5d4d8de4506a441396aa4dbf
gzhu725 <gloriazhu66@gmail.com>
2025-04-13 03:32:35 -0400

    add stuff to home page

--- bitcamp2025/src/app/components/SiteHeader.js
+++ bitcamp2025/src/app/components/SiteHeader.js
@@ -9,14 +9,15 @@ export default function SiteHeader() {
         {/* Centered Block */}
         <div className="flex flex-col items-center text-center w-full">
           <h2 className="w-full text-5xl font-semibold tracking-tight dark:text-white sm:text-7xl">
-            Empower Therapists with{" "}
+            Empower Therapists and Clients with{" "}
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-emerald-500 via-lime-500 to-green-500">
               Therassist
             </span>
           </h2>
 
           <p className="mt-8 max-w-3xl text-xl font-medium text-zinc-400">
-            Therassist description here
+            Be present. Be your best self. Track progress, enhance your mental
+            health journey, and empower yourself—all in one convenient platform.
           </p>
         </div>
       </div>
